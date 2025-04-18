5bcc05469e867f0f5e9f57bf375afa8b2a616821
Matheus Silva <ms25022003@gmail.com>
2025-04-18 19:02:21 -0300

    create project

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from 'next';
+import { Nunito_Sans } from 'next/font/google';
+import './globals.css';
+
+const fontNunitoSans = Nunito_Sans({
+  variable: '--font-nunito-sans',
+  subsets: ['latin'],
+  display: 'swap',
+  style: ['normal', 'italic'],
+  weight: ['200', '300', '400', '500', '600', '700', '800', '900'],
+})
+
+export const metadata: Metadata = {
+  title: 'Buscar perfil GitHub',
+  description: 'Busque o perfil de um usuário do GitHub',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`${fontNunitoSans.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
