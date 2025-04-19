6351d0da39206ea4502ea9cc6cdedac7d7ab7b39
Matheus Silva <ms25022003@gmail.com>
2025-04-19 18:05:47 -0300

    feat: base layout application

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
+import { MainLayout } from '@/layouts';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div>
-      <p>Hello world</p>
-    </div>
+    <MainLayout>
+      <section className='bg-black max-w-6xl w-full py-10 px-6 m-auto h-full flex-1 max-h-[537px]'>
+        <div>
+          <Image className='mx-auto' src="/logotype.svg" width={384} height={82} alt="Logotype application" />
+          <div className='relative mx-auto flex-none max-w-lg w-full mt-7'>
+            <input type='text' placeholder='Digite um usuário do Github' className='rounded-xl w-full text-xl font-semibold py-[17px] pl-4 pr-16 bg-grey-medium' />
+            <button className='absolute right-0 bg-base top-0 h-full w-[62px] rounded-xl border-grey-medium border'>
+              <Image className='m-auto' src="/search.svg" width={25} height={25} alt='Search icon' />
+            </button>
+          </div>
+        </div>
+      </section>
+    </MainLayout>
   );
 }
---
+++ src/layouts/main.layout.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import { ReactNode } from 'react'
+
+type MainLayoutProps = {
+    children: ReactNode
+}
+
+export const MainLayout = ({ children }: MainLayoutProps) => {
+  return (
+    <main className='min-h-dvh relative flex flex-col'>
+      <Image className='fixed overflow-auto -z-10 top-[26px] left-[71px]' src='/grid.svg' width={239} height={255} alt="grid background" />
+      <Image className='fixed overflow-auto -z-10 -top-[268px] -right-[254px]' src='/elipse.svg' width={888} height={888} alt="elipse background" />
+      <Image className='fixed overflow-auto -z-10 -bottom-[62px] -left-[415px]' src='/elipse.svg' width={674} height={674} alt="elipse background" />
+      {children}
+    </main>
+  )
+}
