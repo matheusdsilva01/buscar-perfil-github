5ff3b7e93689419cd7e6f558eb479f93512b27fe
Matheus Silva <ms25022003@gmail.com>
2025-05-04 11:27:28 -0300

    fix: error statement fetch

--- src/requests/searchUser.ts
+++ src/requests/searchUser.ts
@@ -1,4 +1,7 @@
 export async function searchUser(search: string) {
     const response = await fetch(`https://api.github.com/users/${search}`)
-    return response.json()
+    if (!response.ok) {
+        throw new Error('Something went wrong')
+    }
+    return await response.json()
 }
